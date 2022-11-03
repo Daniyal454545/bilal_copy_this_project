4d8ae62760f2222b30a1c057d622041219f60f0d
Daniyal454545 <daniyalkozhokmatov@gmail.com>
2022-11-03 17:56:18 +0600

    doni

---
+++ account/send_email.py
@@ -0,0 +1,14 @@
+from django.core.mail import send_mail
+
+
+def send_confirmation_email(user, code):
+    full_link = f'http://34.28.220.66/accounts/activate/{code}/'
+    send_mail(
+        'Hello! Activate your account!',
+        f'To activate your account you need to link: {full_link}',
+        'abdb2226@gmail.com',
+        [user],
+        fail_silently=False
+    )
+
+
---
+++ crud/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from account.models import CustomUser
+
+
+
+class Person(models.Model):
+    name = models.ForeignKey(CustomUser,  max_length=100, on_delete=models.CASCADE) #имя
+    age = models.CharField('возраст', max_length=100) #возраст
+    height = models.CharField('выстота', max_length=10, blank=True) #выстота
+    weight = models.CharField('вес', max_length=100, blank=True) #вес
+    blood_type = models.CharField('группа крови', max_length=10, blank=True) #группа крови
+    allergy = models.CharField('аллергия на что либо', max_length=100, blank=True) #аллергия на что либо
+    symptoms = models.CharField('симптомы', max_length=500, blank=True) #симптомы
+    disability = models.CharField('инвалидность', max_length=100, blank=True) #инвалидность
+    injury = models.CharField('травма', max_length=100, blank=True)
+    illness = models.CharField('болезнь', max_length=100) #болезнь
+    person_images = models.ImageField('фотография', upload_to='person_image/', blank=True)
+
+
+    def __str__(self):
+        return f'{self.name} - {self.last_name}'
